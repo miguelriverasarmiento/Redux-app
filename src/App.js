6b8231e7962242b98aefe17bc7101eb3b8bbba4a
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2022-12-30 08:16:00 -0500

    useReducer introduccion

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
-const counterReducer = (state, action) => state
+const counterReducer = (state, action) => {
+  
+  const {type} = action
+  if (type === '@counter/incremented') {
+    return state + 1
+  }
+  if (type === '@counter/decremented') {
+    return state - 1
+  }
+  if (type === '@counter/reseted') {
+    return 0
+  }
+}
 
-counterReducer(0, "Incrementar")
+const actionIncrement = {
+  type: '@counter/incremented'
+}
+
+const actionDecremented = {
+  type: '@counter/decremented'
+}
+
+const actionReset = {
+  type: '@counter/reseted'
+}
+
+export default function App() {
+
+  return (
+    <div>
+      {counterReducer(0, actionIncrement)}
+      {counterReducer(1, actionDecremented)}
+    </div>
+
+  );
+}
