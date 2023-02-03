67b65a0b7f75ed83cd76469d069b262d4ec59e6b
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-02-03 11:05:32 -0500

    I add PostAuthor component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,13 +1,17 @@
 import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
 
 const PostsList = () => {
     const posts = useSelector(selectAllPosts)
 
-    const renderedPosts = posts.map(post => (
+     const renderedPosts = posts.map(post => (
         <article key={post.id}>
             <h3>{post.title}</h3>
             <p>{post.content.substring(0, 100)}</p>
+            <p className="postCredit">
+                <PostAuthor userId={post.userId} />
+            </p>
         </article>
     ))
 
@@ -19,3 +23,4 @@ const PostsList = () => {
     )
 }
 export default PostsList
+
