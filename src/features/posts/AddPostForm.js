f124af09ed173d5c3d3bba813c2af6d40cfafc9e
miguelriverasarmiento <miguelriverasarmiento@gmail.com>
2023-02-07 16:53:32 -0500

    Finish of redux-app

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -36,7 +36,7 @@ const AddPostForm = () => {
     ))
     return(
         <section>
-            <h2>Add a New Post</h2>
+            <h2>Add a New Post Here</h2>
             <form>
                 <label htmlFor="postTitle">Post Title:</label>
                 <input
